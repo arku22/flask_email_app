0e832357d2621eb2d76e955381e5e17cfa0639c7
archit kumar <archit@wpi.edu>
2023-03-25 17:03:57 -0500

    Use 'Flask-Postmark' instead of 'postmarker'

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
-from postmarker.core import PostmarkClient
-from dotenv import load_dotenv
+from flask import Flask
+from flask_postmark import Postmark
 import os
 
 
-# load env variables
-load_dotenv()
+# init flask app
+app = Flask(__name__)
 
-postmark = PostmarkClient(server_token=os.environ.get('email_server_token'))
-postmark.emails.send(
-    From=os.environ.get('sender_email'),
-    To=os.environ.get('receiver_email'),
-    Subject='Hello from Postmark',
-    HtmlBody='<strong>Hello</strong> dear postmark user.'
-)
+# set config vars
+app.config["POSTMARK_SERVER_TOKEN"] = os.environ.get("email_server_token")
+postmark = Postmark(app)
+
+@app.route("/send", methods=["GET"])
+def send_email():
+    postmark.send(
+        From=os.environ.get("sender_email"),
+        To=os.environ.get("receiver_email"),
+        Subject="Postmark Test",
+        HtmlBody="<html><body>This is a test email</body></html>"
+    )
+
+    return "OK"
